9ab83795e813668fbf2f192138cb7cd8153b0fb5
Harsh Gautam <hgautam208@gmail.com>
2021-09-07 00:09:53 +0530

    save form data to local storage

--- js/newbook.js
+++ js/newbook.js
@@ -6,6 +6,19 @@ form.addEventListener("submit", (e) => {
   const title = e.target.title.value;
   const author = e.target.author.value;
   const pages = e.target.pages.value;
-  const readStatus = e.target.readstatus.value;
-  console.log(title, author, pages, readStatus);
+  const readStatus = e.target.readstatus.value === "yes" ? true : false;
+  saveData({ title, author, pages, readStatus });
+  alert("Book saved to library!");
+  // TODO: clear form inputs
 });
+
+function saveData(book) {
+  let data = localStorage.getItem("data");
+  if (data === null) {
+    localStorage.setItem("data", JSON.stringify([book]));
+    return;
+  }
+  data = JSON.parse(data);
+  data.push(book);
+  localStorage.setItem("data", JSON.stringify(data));
+}
