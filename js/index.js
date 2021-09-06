3836ba86f719f6c2a5e650a2af26bbb7cc458d0c
Harsh Gautam <hgautam208@gmail.com>
2021-09-06 22:52:40 +0530

    selecting table body instead of whole table element

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function populateTable() {
   const booksData = JSON.parse(localStorage.getItem("data"));
   if (booksData === null) alert("Failed to retrieve books data");
-  const table = document.querySelector(".books-table");
+  const table = document.querySelector(".books-table > tbody");
   for (let book in booksData) {
     const trTag = createTag(book);
     table.appendChild(trTag);
